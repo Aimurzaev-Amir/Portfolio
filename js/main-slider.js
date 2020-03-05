de4a20d9c014a80dd14f4880dbceb7cd128c3004
Aimurzaev-Amir <aimurzaev@mail.ru>
2020-03-05 16:10:13 +0600

    Сверстал страницу, без адаптива, перехожу к анимации

---
+++ js/main-slider.js
@@ -0,0 +1,9 @@
+// var slide = document.getElementById('slide')
+// window.onmousemove = function(e){
+//     var x = e.clientX;
+//     slide.style.left = x+'px';
+// }
+// window.onmouseout = function(e){
+//     var x = e.clientX;
+//     slide.style.left = x;
+// }
