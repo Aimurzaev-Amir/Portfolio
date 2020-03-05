1f5c356491ab080848e5745a10f9bb62c86b1aca
Aimurzaev-Amir <aimurzaev@mail.ru>
2020-03-06 00:04:25 +0600

    Обложка для страницы About Me

---
+++ js/typing.js
@@ -0,0 +1,8 @@
+$('document').ready(function(){
+    var typed = new Typed ('#typed',{
+        stringsElement: '#typed-strings',
+        loop: true,
+        backSpeed: 40,
+        typeSpeed: 40
+    })
+});
