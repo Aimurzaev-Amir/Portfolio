2f9cb48e86d5990515e439805ad72040db5711f0
Aimurzaev-Amir <aimurzaev@mail.ru>
2020-03-25 04:08:25 +0600

    articles published... first one is not done

--- js/main.js
+++ js/main.js
@@ -53,7 +53,17 @@ $(function() {
     loop: true,
   });
 
+// Share article buttons
+Share = {
+  me : function(el){
+      Share.popup(el.href);
+      return false;
+  },
 
+  popup: function(url) {
+      window.open(url,'','toolbar=0,status=0,width=626,height=436');
+  }
+};
   
   
   
