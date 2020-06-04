2e03c0310df4db54b6cc1955c97fd13a0d82ffbc
Aimurzaev-Amir <aimurzaev@mail.ru>
2020-06-04 22:37:24 +0600

    changed html

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-// toggle for adaptive mobile devices
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+// // toggle for adaptive mobile devices
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+// const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  links.forEach(link => {
-    link.classList.toggle("fade");
-  });
-});
+// hamburger.addEventListener("click", () => {
+//   navLinks.classList.toggle("open");
+//   links.forEach(link => {
+//     link.classList.toggle("fade");
+//   });
+// });
 
 
 // typing animation function on the main page
@@ -32,26 +32,7 @@ $(function() {
     });
   });
   
-  // Swiper slider on the Portfolio page
-  var swiper = new Swiper('.swiper-container', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    spaceBetween: 100,
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows : false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-    loop: true,
-  });
+
 
 // Share article buttons
 Share = {
