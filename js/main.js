ac81213e57998bff0e742180b77ab9a8d463df6f
Aimurzaev-Amir <aimurzaev@mail.ru>
2020-06-12 00:19:51 +0600

    removed delay on animation when screen < 768

--- js/main.js
+++ js/main.js
@@ -1,36 +1,43 @@
+if (document.documentElement.clientWidth < 768) {
+  var skillCard = document.querySelectorAll(".skill-card");
+  for (var i = 0; i <= skillCard.length; i++) {
+    skillCard[i].removeAttribute("data-aos-delay");
+  }
+}
+
+console.log(document.documentElement.clientWidth);
 // Scroll animation on the main page
-$(function() {
-    $("#scroll-indicator").click(function() {
-      $("html, body").animate({
-        scrollTop: $("#intro").offset().top - 50
+$(function () {
+  $("#scroll-indicator").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $("#intro").offset().top - 50,
       },
-      'slow'
-      );
-    });
+      "slow"
+    );
   });
-  
+});
+
 // Share article buttons
 Share = {
-  me : function(el){
-      Share.popup(el.href);
-      return false;
+  me: function (el) {
+    Share.popup(el.href);
+    return false;
   },
 
-  popup: function(url) {
-      window.open(url,'','toolbar=0,status=0,width=626,height=436');
-  }
+  popup: function (url) {
+    window.open(url, "", "toolbar=0,status=0,width=626,height=436");
+  },
 };
 
 // Setting the viewport height
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 // on viewport height change
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
-
-
 
 // Modal Gallery
 var modal = document.getElementById("myModal");
@@ -43,13 +50,13 @@ function changeSlideIndex(x) {
 var img = document.querySelectorAll(".myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-function showModal(slideIndex){
+function showModal(slideIndex) {
   modal.style.display = "block";
   modalImg.src = img[slideIndex].src;
   captionText.innerHTML = img[slideIndex].alt;
 }
 // close modal gallery
 var span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
