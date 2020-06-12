611b1472fe14777241cf2d505340fc924331563a
Aimurzaev-Amir <aimurzaev@mail.ru>
2020-06-12 22:48:51 +0600

    add buttons to photo  gallery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// mobile devices animation turn off
 if (document.documentElement.clientWidth < 768) {
   var skillCard = document.querySelectorAll(".skill-card");
   for (var i = 0; i <= skillCard.length; i++) {
@@ -5,7 +6,15 @@ if (document.documentElement.clientWidth < 768) {
   }
 }
 
-console.log(document.documentElement.clientWidth);
+// Setting the viewport height
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+// on viewport height change
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+
 // Scroll animation on the main page
 $(function () {
   $("#scroll-indicator").click(function () {
@@ -30,31 +39,46 @@ Share = {
   },
 };
 
-// Setting the viewport height
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-// on viewport height change
-window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
-
 // Modal Gallery
 var modal = document.getElementById("myModal");
-var slideIndex = 0;
-function changeSlideIndex(x) {
-  slideIndex = x;
-  console.log(slideIndex);
-  showModal(slideIndex);
-}
 var img = document.querySelectorAll(".myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-function showModal(slideIndex) {
-  modal.style.display = "block";
+
+var slideIndex = 0;
+function changeSlideIndex(x) {
+  slideIndex = x;
+  showModal(slideIndex);
+}
+function getPhoto() {
   modalImg.src = img[slideIndex].src;
   captionText.innerHTML = img[slideIndex].alt;
 }
+function showModal(slideIndex) {
+  modal.style.display = "block";
+  getPhoto();
+}
+//prev-next photo buttons
+var nextPhoto = document.getElementById("nextPhoto");
+var prevPhoto = document.getElementById("prevPhoto");
+nextPhoto.addEventListener("click", nextPhotofunc);
+prevPhoto.addEventListener("click", prevPhotofunc);
+
+function nextPhotofunc() {
+  slideIndex += 1;
+  if (slideIndex > img.length - 1) {
+    slideIndex = 0;
+  }
+  getPhoto();
+}
+function prevPhotofunc() {
+  slideIndex -= 1;
+  if (slideIndex < 0) {
+    slideIndex = img.length - 1;
+  }
+  getPhoto();
+}
+
 // close modal gallery
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
